d068e9bcc0d3b84fef710f38bc4df99ea2c6cfd4
xhasak <456221@mail.muni.cz>
2020-01-31 19:02:58 +0100

    Implemented scheduled method checkTime with fixedRate 5 seconds

--- src/main/java/sk/tuke/fei/hasak/istimeservice/scheduling/ScheduledTask.java
+++ src/main/java/sk/tuke/fei/hasak/istimeservice/scheduling/ScheduledTask.java
@@ -1,7 +1,19 @@
 package sk.tuke.fei.hasak.istimeservice.scheduling;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Šimon Hašák
  */
+@Slf4j
+@Component
 public class ScheduledTask {
+
+    @Scheduled(fixedRate = 5000)
+    public void chechTime() {
+        log.info("Vypis");
+    }
+
 }
