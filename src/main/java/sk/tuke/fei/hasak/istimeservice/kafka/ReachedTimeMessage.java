7952a5508ba5d36f405e10872ae56dbcda744a63
Simon Hasak <simon.hasak@gmail.com>
2020-03-14 11:53:17 +0100

    Class for exchanging data between microservices in json format

--- src/main/java/sk/tuke/fei/hasak/istimeservice/kafka/ReachedTimeMessage.java
+++ src/main/java/sk/tuke/fei/hasak/istimeservice/kafka/ReachedTimeMessage.java
@@ -1,4 +1,16 @@
 package sk.tuke.fei.hasak.istimeservice.kafka;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReachedTimeMessage {
+
+    private long id;
+
 }
