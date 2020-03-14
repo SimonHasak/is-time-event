12acea07eddda1d410776cc70c6addfe0b3ff7bf
Simon Hasak <simon.hasak@gmail.com>
2020-03-14 11:52:51 +0100

    Deserializer for SavedEventMessage

--- src/main/java/sk/tuke/fei/hasak/istimeservice/kafka/SavedEventMessageDeserializer.java
+++ src/main/java/sk/tuke/fei/hasak/istimeservice/kafka/SavedEventMessageDeserializer.java
@@ -1,4 +1,8 @@
 package sk.tuke.fei.hasak.istimeservice.kafka;
 
-public class SavedEventMessageDeserializer {
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+public class SavedEventMessageDeserializer extends JsonDeserializer<SavedEventMessage> {
+
+    public SavedEventMessageDeserializer() { super(SavedEventMessage.class); }
 }
