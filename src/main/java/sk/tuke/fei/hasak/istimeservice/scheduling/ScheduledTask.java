43e05d9ec6d80fab8f99d9a9547df4cdd4763be8
Simon Hasak <simon.hasak@gmail.com>
2020-03-14 11:51:05 +0100

    Added publishing of events which reached desired time

--- src/main/java/sk/tuke/fei/hasak/istimeservice/scheduling/ScheduledTask.java
+++ src/main/java/sk/tuke/fei/hasak/istimeservice/scheduling/ScheduledTask.java
@@ -1,8 +1,15 @@
 package sk.tuke.fei.hasak.istimeservice.scheduling;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import sk.tuke.fei.hasak.istimeservice.kafka.ReachedTimeMessage;
+import sk.tuke.fei.hasak.istimeservice.kafka.ReachedTimeProducer;
+import sk.tuke.fei.hasak.istimeservice.service.IsTimeService;
+
+import java.time.LocalDateTime;
+import java.util.stream.StreamSupport;
 
 /**
  * @author Šimon Hašák
@@ -11,8 +18,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduledTask {
 
-    @Scheduled(fixedRate = 5000)
-    public void checkTime() {
+    private final ReachedTimeProducer producer;
+
+    private final IsTimeService isTimeService;
+
+    @Autowired
+    public ScheduledTask(ReachedTimeProducer producer, IsTimeService isTimeService) {
+        this.producer = producer;
+        this.isTimeService = isTimeService;
+    }
+
+    @Scheduled(fixedRate = 60_000)
+    public void checkEventTime() {
+//        StreamSupport.stream(isTimeService.findAll().spliterator(), false)
+        isTimeService.findAll().forEach(event -> {
+            if (event.getTime().isAfter(LocalDateTime.now())) {
+                this.producer.sendReachTimeMessage(new ReachedTimeMessage(event.getId()));
+                this.isTimeService.deleteById(event.getId());
+            }
+        });
         log.info("vypis");
     }
 
