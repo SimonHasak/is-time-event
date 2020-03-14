979f09491160611cb8b2d328fecfe2fae7176a6a
Simon Hasak <simon.hasak@gmail.com>
2020-03-14 11:52:09 +0100

    Added consumer for SavedEventMessage

--- src/main/java/sk/tuke/fei/hasak/istimeservice/kafka/SavedEventMessageKafkaListener.java
+++ src/main/java/sk/tuke/fei/hasak/istimeservice/kafka/SavedEventMessageKafkaListener.java
@@ -1,10 +1,27 @@
 package sk.tuke.fei.hasak.istimeservice.kafka;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import sk.tuke.fei.hasak.istimeservice.model.TimeEvent;
+import sk.tuke.fei.hasak.istimeservice.service.IsTimeService;
 
+@Slf4j
 @Component
-public class SavedMessageKafkaListener {
+public class SavedEventMessageKafkaListener {
 
+    private final IsTimeService isTimeService;
 
+    @Autowired
+    public SavedEventMessageKafkaListener(IsTimeService isTimeService) {
+        this.isTimeService = isTimeService;
+    }
+
+    @KafkaListener(topics = "${kafka.saved.event}", groupId = "${kafka.group.saved.event}")
+    public void processSavedEventMessage(SavedEventMessage message) {
+        log.info("[Is-Time-Service] received:{}", message);
+        isTimeService.save(new TimeEvent(message.getTime()));
+    }
 
 }
