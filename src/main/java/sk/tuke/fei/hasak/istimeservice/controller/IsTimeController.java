2f8b7695a07d6fb7f3ada146e8e83d18719b6345
Simon Hasak <simon.hasak@gmail.com>
2020-04-10 23:45:10 +0200

    Added cross origin to frontend

--- src/main/java/sk/tuke/fei/hasak/istimeservice/controller/IsTimeController.java
+++ src/main/java/sk/tuke/fei/hasak/istimeservice/controller/IsTimeController.java
@@ -24,6 +24,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
  *
  * @author Šimon Hašák
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/is-time")
 public class IsTimeController {
